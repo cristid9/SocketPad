d77444c11d98b7363013de3c46822cf9a7ffb64a
Cristi D <cristi.dnla@gmail.com>
2017-02-10 21:14:52 +0200

    Added class for file management

---
+++ server/FilesManager.cpp
@@ -0,0 +1,88 @@
+//
+// Created by cristi on 26.01.17.
+//
+
+#include "FilesManager.h"
+#include <soci/soci.h>
+#include <soci/sqlite3/soci-sqlite3.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+using namespace soci;
+
+static std::string FilesManager::storage_path = nullptr;
+
+static void FilesManager::load_values(std::string storage_pth)
+{
+    FilesManager::storage_path = storage_pth;
+}
+
+static std::string FilesManager::create_empty_file(std::string username, std::string filename)
+{
+
+    std::string file_path = storage_path + "/" + username + "/" + filename;
+
+    if (!FilesManager::check_storage_exists(username))
+    {
+        FilesManager::add_user_directory(username);
+    }
+
+    if (!FilesManager::check_file_exists(username, filename))
+    {
+        creat(file_path.c_str(), 0700);
+    }
+
+    return file_path;
+}
+
+static bool FilesManager::check_file_exists(std::string username, std::string filename)
+{
+    std::string candidate_path = storage_path + "/" + username + "/" + filename;
+
+    struct stat st = {0};
+
+    if (stat(candidate_path.c_str(), &st) == -1)
+        return false;
+    return true;
+}
+
+static bool FilesManager::check_storage_exists(std::string username)
+{
+    std::string candidate_path = storage_path + "/" + username;
+
+    struct stat st = {0};
+
+    if (stat(candidate_path.c_str(), &st) == -1)
+        return false;
+    return true;
+}
+
+static void FilesManager::add_user_directory(std::string username)
+{
+    std::string candidate_path = storage_path + "/" + username;
+    mkdir(candidate_path.c_str(), 0700);
+}
+
+
+FilesManager::FilesManager()
+{
+}
+
+std::vector<std::string> FilesManager::get_user_files(std::string username)
+{
+    session sql(sqlite3, "dbname=" + db_path);
+
+    int creator_id;
+
+    sql << "select id from User where username=:username",
+        use(username), into(creator_id);
+
+    std::vector<std::string> files(100);
+
+    sql << "select name from File where creator_id=:creator_id",
+        use(creator_id), into(files);
+
+    return files;
+}
+
---
+++ server/FilesManager.h
@@ -0,0 +1,71 @@
+//
+// Created by cristi on 26.01.17.
+//
+
+#ifndef SERVER_FILESMANAGER_H
+#define SERVER_FILESMANAGER_H
+
+#include <string>
+#include <vector>
+#include "User.h"
+
+class FilesManager
+{
+private:
+    /**
+     * @brief Initialized with a dummy value. You must call `load_values` before using any other
+     *        methods that rely on this member.
+     */
+    static std::string storage_path;
+
+    /**
+     * @brief Helper that checks if the storage dir for this user exists.
+     *
+     * @param username The user whose storage dir we are targeting.
+     * @return `true` if it exists, `false` otherwise.
+     */
+    static bool check_storage_exists(std::string username);
+
+    /**
+     * @brief Check if a file is in the user's directory.
+     *
+     * @param username The targeted user.
+     * @param filename The targeted file.
+     * @return `true` if it exists or `false` if it doesn't.
+     */
+    static bool check_file_exists(std::string username, std::string filename);
+public:
+
+    FilesManager();
+
+    // move this to file.cpp
+
+    /**
+     * @brief Creates the file with `filename` name in the container associated with the
+     *        user `username` on the disk.
+     * @param username The `name` of the author.
+     * @param filename The `name` of the file.
+     * @return Returns a string representing the path to this file.
+     */
+    static std::string create_empty_file(std::string username, std::string filename);
+
+
+    /**
+     * @brief Creates a directory in the storage location for he specified user.
+     *
+     * @param username The `username` of the user.
+     */
+    static void add_user_directory(std::string username);
+
+
+    /**
+     * @brief You'll probably use this after you've loaded the config into a json
+     *        object.
+     *
+     * @param storage_pth The pathe to the storage dir.
+     */
+    static void load_values(std::string storage_pth);
+};
+
+
+#endif //SERVER_FILESMANAGER_H
