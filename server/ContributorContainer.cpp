328eaf69eaece781c0218c624e0f63e0e0e2334f
Cristi D <cristi.dnla@gmail.com>
2017-02-13 00:14:48 +0200

    Added basic implementation for the ContributorContainer class

--- server/ContributorContainer.cpp
+++ server/ContributorContainer.cpp
@@ -1,1 +1,21 @@
 #include "ContributorContainer.h"
+
+ContributorContainer::ContributorContainer(unsigned int cid, std::string cusername, CNSocket csock)
+    : id(cid), username(cusername), sock(csock)
+{
+}
+
+unsigned int ContributorContainer::get_id() const
+{
+    return id;
+}
+
+std::string ContributorContainer::get_username() const
+{
+    return username;
+}
+
+CNSocket ContributorContainer::get_sock() const
+{
+    return sock;
+}
--- server/ContributorContainer.h
+++ server/ContributorContainer.h
@@ -2,10 +2,41 @@
 #ifndef SERVER_CONTRIBUTORCONTAINER_H
 #define SERVER_CONTRIBUTORCONTAINER_H
 
+#include "CNSocket.h"
+#include <string>
 
 class ContributorContainer
 {
+private:
+    unsigned int id;
+    std::string username;
+    CNSocket sock;
+public:
 
+    /**
+     * @param cid The `id` of the newly created contributor.
+     * @param cusername The `username` of the newly created username.
+     * @param csock Socket to the clinet opened by this contributor.
+     */
+    ContributorContainer(unsigned int cid, std::string cusername, CNSocket csock);
+
+    /**
+     * @brief Getter for the `id` field.
+     * @return The `id` of this contributor.
+     */
+    unsigned int get_id() const;
+
+    /**
+     * @brief Getter for `username` field.
+     * @return The `username` of this contributor.
+     */
+    std::string get_username() const;
+
+    /**
+     * @brief Getter for `sock` field.
+     * @return The socket connected to this user's client.
+     */
+    CNSocket get_sock() const;
 };
 
 
