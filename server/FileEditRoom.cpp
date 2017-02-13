d9cee6f27743547ce84fd793f09831518eeba488
Cristi D <cristi.dnla@gmail.com>
2017-02-13 02:23:22 +0200

    Added method for propagating changes among the connected users

--- server/FileEditRoom.cpp
+++ server/FileEditRoom.cpp
@@ -1,1 +1,39 @@
 #include "FileEditRoom.h"
+#include <algorithm>
+
+FileEditRoom::FileEditRoom()
+{
+}
+
+FileEditRoom::FileEditRoom(std::string flnm, unsigned int fid)
+    : filename(flnm), file_id(fid)
+{
+}
+
+void FileEditRoom::add_contributor(ContributorContainer ctb)
+{
+    contributors.push_back(ctb);
+}
+
+void FileEditRoom::remove_contributor(unsigned int id)
+{
+    contributors.erase(
+            std::remove_if(
+                    contributors.begin(),
+                    contributors.end(),
+                    [=](ContributorContainer ctb) { return ctb.get_id() == id; }
+            )
+    );
+}
+
+void FileEditRoom::propaget_change(unsigned int author_id, std::string change)
+{
+    // Ideally each message should be sent in a differnt thread
+    for (auto ctb : contributors)
+    {
+        if (ctb.get_id() != author_id)
+        {
+            ctb.get_sock().send_message(change);
+        }
+    }
+}
--- server/FileEditRoom.h
+++ server/FileEditRoom.h
@@ -1,9 +1,62 @@
 #ifndef SERVER_FILEEDITROOM_H
 #define SERVER_FILEEDITROOM_H
 
+#include <vector>
+#include <utility>
+#include <string>
+#include "ContributorContainer.h"
 
-class FileEditRoom {
+class FileEditRoom
+{
+private:
+    /**
+     * @brief Container of contributors.
+     */
+    std::vector<ContributorContainer> contributors;
 
+    /**
+     * @brief The of the `filename`.
+     */
+    std::string filename;
+
+    /**
+     * @brief The `id` of the edited file.
+     */
+    unsigned int file_id;
+public:
+
+    /**
+     * @param flnm `name` of the edited file.
+     * @param fid `id` of the edited file
+     */
+    FileEditRoom(std::string flnm, unsigned int fid);
+
+    /**
+     * @brief No arguments constructor.
+     */
+    FileEditRoom();
+
+    /**
+     * @brief Setter for the `contributors` field.
+     *
+     * @param ctb A new contributor.
+     */
+    void add_contributor(ContributorContainer ctb);
+
+    /**
+     * @brief Removes a contributor from this room.
+     *
+     * @param id The `id` of the targeted contributor.
+     */
+    void remove_contributor(unsigned int id);
+
+    /**
+     * brief Propagates a change made by 1 contributor to the others.
+     *
+     * @param author_id Author of the change.
+     * @param change Serialized json contining the
+     */
+    void propaget_change(unsigned int author_id, std::string change);
 };
 
 
